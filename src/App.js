9ac8a66f7d6320b08140c817b065c07bc5c733e3
Dejan Conkic <conkicdejan@gmail.com>
2022-06-24 12:41:30 +0200

    Zadatak 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AppCustomers from "./pages/AppCustomers";
+import AppProducts from "./pages/AppProducts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/customers">Customers</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/customers">
+            <AppCustomers />
+          </Route>
+          <Route path="/products">
+            <AppProducts />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
