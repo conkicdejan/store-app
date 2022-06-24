ec5dc297f67a2632cc23222b0500fe1c0fb054a2
Dejan Conkic <conkicdejan@gmail.com>
2022-06-24 16:16:51 +0200

    Zadatak 6

--- src/pages/AppProducts.jsx
+++ src/pages/AppProducts.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 import Product from "../components/ProductsTable";
 import ProductService from "../services/ProductService";
-import ProductsTable from './../components/ProductsTable';
+import ProductsTable from "./../components/ProductsTable";
 
 export default function AppProducts() {
   const [products, setProducts] = useState(ProductService.getAll());
+  const [search, setSearch] = useState("");
+
+  const productsList = () =>
+    products.filter((product) => product.name.toLowerCase().includes(search));
 
   return (
     <div>
-      <h3>Product list</h3>
-      <ProductsTable products={products} />
+      <div>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="search..."
+        />
+        <button onClick={() => setSearch('')}>clear</button>
+      </div>
+      <div>
+        <h3>Product list</h3>
+        <ProductsTable products={productsList()} />
+      </div>
     </div>
   );
 }
